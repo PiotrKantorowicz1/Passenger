a452d357a4cb8db2a6a84d60f0ec5c31bc77ca56
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2017-09-14 00:43:38 +0200

    StartModelClassesAdded

---
+++ Passenger.Core/Domain/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace Passenger.Core.Domain
+{
+    public class Node
+    {
+        public string Address { get; protected set; }
+
+        public string Longitude { get; protected set; }
+
+        public string Latitude { get; protected set; }
+
+    }
+}
